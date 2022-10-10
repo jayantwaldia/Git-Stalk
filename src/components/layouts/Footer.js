fe2e4510c216a53a263e4f0b96510a795b375faa
José Mourinho <115450953+josemourn@users.noreply.github.com>
2022-10-10 12:36:06 +0530

    Update Footer.js

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer w-[90%] md:w-[100%] mx-auto p-4 rounded-xl bg-black text-primary-content footer-center">
       <div className="flex justify-between w-[80%] md:w-[50%] ">
-        <a href="https://github.com/jayantwaldia/Git-Stalk">
+        <a href="https://github.com/jayantwaldia/Git-Stalk" target="_blank">
           <FaGithub size={25} />
         </a>
         <a href="https://www.linkedin.com/in/jayantwaldia/">
